3fe5de89c536e069509fab95e9b30c3c6148b519
Ahmed Masood <ahmedandmehraj@icloud.com>
2022-04-05 14:40:15 +0100

    work

--- python.py
+++ python.py
@@ -2,6 +2,6 @@ import os
 list1 = [5, 9, 4, 34, 17, 3, 10]
 list1.reverse()
 print("Reversed list = ", list1)
-list2 = list1.copy()
+list2 = list(list1)
 list2.sort()
 print("Sorted list = ", list2)
