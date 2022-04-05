ccb4a53c6477c3bc673181b6df52fe3fef442916
Ahmed Masood <ahmedandmehraj@icloud.com>
2022-04-05 14:23:50 +0100

    work

--- python.py
+++ python.py
@@ -2,7 +2,5 @@ import os
 list1 = [5, 9, 4, 34, 17, 3, 10]
 list2 = list1.reverse()
 list3 = list1.sort()
-indexedv = list2[2:6]
 print("Reversed list = ", list2)
 print("Sorted list = ", list3)
-print("Indexed values = ", indexedv)
